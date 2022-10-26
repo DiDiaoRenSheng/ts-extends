b21574e0760378d598827fbe59a6b0f26e63f23f
爱咕咕的布咕鸟 <1115629735@qq.com>
2022-10-26 17:01:49 +0800

    基础数组尝试封装

--- basic-type-extension/src/implements/array.implement.ts
+++ basic-type-extension/src/implements/array.implement.ts
@@ -4,4 +4,43 @@ Array.prototype.Add = function Add(inputArray) {
   this.push(inputArray);
 };
 
+Array.prototype.ToList = function () {
+  return new List(this);
+};
+
 //#endregion
+
+export class List<T> {
+  private _items: T[];
+  private static readonly _emptyArray: [] = [];
+
+  public set Capacity(v: number) {
+    this.Capacity = v;
+  }
+
+  public get Capacity(): number {
+    return this.Capacity;
+  }
+
+  public get Count(): number {
+    return this.Count;
+  }
+
+  constructor();
+  constructor(parameterType: number);
+  constructor(parameterType: T[]);
+  constructor(parameterType?: number | T[]) {
+    if (parameterType instanceof Number) {
+    } else if (parameterType instanceof Array) {
+    } else {
+      this._items = List._emptyArray;
+    }
+    this._items = List._emptyArray;
+  }
+
+  Add(item: T): void {}
+
+  AddRange(collection: T[]): void {}
+
+  Clear(): void {}
+}
--- basic-type-extension/src/interfaces/array.interface.ts
+++ basic-type-extension/src/interfaces/array.interface.ts
@@ -1,7 +1,11 @@
+import { List } from "../implements/array.implement";
+
 //String类型扩展接口声明【全局类型声明合并】
 declare global {
   interface Array<T> {
-    Add(inputArray: T): void;
+    Add(inputArrayValue: T): void;
+
+    ToList(): List<T>;
   }
 }
 export {};
